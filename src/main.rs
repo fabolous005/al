72f30ff2da2dc59df2322df44a04a523378c7bbe
Fabian <fabolous.e05@gmail.com>
2023-10-03 20:14:30 +0200

    add canvas library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use draw::*;
+
 fn main() {
-    println!("Hello, world!");
+    // create a canvas to draw on
+    let mut canvas = Canvas::new(100, 100);
+
+    // create a new drawing
+    let mut rect = Drawing::new()
+        // give it a shape
+        .with_shape(Shape::Rectangle {
+            width: 50,
+            height: 50,
+        })
+        // move it around
+        .with_xy(25.0, 25.0)
+        // give it a cool style
+        .with_style(Style::stroked(5, Color::black()));
+
+    // add it to the canvas
+    canvas.display_list.add(rect);
+
+    // save the canvas as an svg
+    render::save(&canvas, "test.svg", SvgRenderer::new()).expect("Failed to save");
 }
